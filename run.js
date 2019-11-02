28a67b7e66a83f86f181ef9414b0b5cd2c95aa80
okuoku <mjt@cltn.org>
2019-11-02 16:14:30 +0900

    Always take the last stream

--- run.js
+++ run.js
@@ -13,31 +13,42 @@ const files = [];
 entries.forEach(e => {
     let entry = false;
     const url = new URL(e.request.url);
-    const name = url.pathname;
+    let name = url.pathname;
     const basename = path.basename(url.pathname);
+    const reListMimeType = RegExp("application.x-mpegurl", "i");
     if(e.response.content.text){
         if(e.response.content.encoding == "base64"){
             entry = new Buffer.from(e.response.content.text, "base64");
         }else{
             entry = e.response.content.text;
         }
+        // Override filename for .m3u8
+        if(reListMimeType.exec(e.response.content.mimeType)){
+            console.log("MIME", e.response.content.mimeType);
+            name += ".m3u8";
+            console.log("NAME", name);
+        }
     }
     files.push({name: name, ext: path.extname(name), 
                basename: basename,
-               blob: entry});
+               blob: entry,
+               url: url.pathname
+    });
     urls[url] = entry;
 });
 
 // Extract key, modified .m3u8
 files.forEach(e => {
-    const reKeyUrl = RegExp('#EXT-X-KEY:METHOD=AES-128,URI="([^"]+)"');
+    const reKeyUrl = RegExp('#EXT-X-KEY:METHOD=AES-128,URI="([^"]+)"',"g");
     if(e.ext == ".m3u8"){
         if(e.blob instanceof Buffer){
             e.blob = e.blob.toString("utf8");
         }
-        const keyUrlArr = reKeyUrl.exec(e.blob);
-        if(reKeyUrl.exec(e.blob)){
-            const keyUrl = keyUrlArr[1];
+        const keyUrlArr = [...e.blob.matchAll(reKeyUrl)];
+        console.log("KEYURLS", keyUrlArr.map(e => e[1]));
+
+        if(keyUrlArr.length > 0){
+            const keyUrl = keyUrlArr[keyUrlArr.length - 1][1];
             if(urls[keyUrl]){
                 console.log(keyUrl);
                 console.log(urls[keyUrl]);
@@ -45,16 +56,26 @@ files.forEach(e => {
                     e.blob.replace(reKeyUrl, "#EXT-X-KEY:METHOD=AES-128,URI=zzzKEY");
                 fs.writeFileSync("out.m3u8", out);
                 fs.writeFileSync("zzzKey", urls[keyUrl]);
+                console.log("LIST", e.url);
+                console.log("KEY", keyUrl);
             }else{
                 console.log("Not found, skip:", keyUrl);
             }
+        }else{
+            console.log("IGNORED", e.blob);
         }
     }
 });
 
 // Extract *.ts
+const done = [];
 files.forEach(e => {
     if(e.ext == ".ts"){
         fs.writeFileSync(e.basename, e.blob);
+        if(done[e.basename]){
+            console.log("OVERWRITE", done[e.basename], e.url);
+        }else{
+            done[e.basename] = e.url;
+        }
     }
 });
